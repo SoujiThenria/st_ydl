92ca9ab47042fc760cb83e0f70cf9feaf152d703
souji <souji@souji-thenria.net>
2021-03-30 20:08:17 +0200

    clean up code and add comments

--- src/config.cpp
+++ src/config.cpp
@@ -6,19 +6,27 @@
 void config::read(std::string _file)
 {
     std::ifstream conf_file;
-    conf_file.open(_file);
     std::string line;
+    std::string save_path;
+    std::vector<std::string> links;
+
+    // open the file
+    conf_file.open(_file);
 
     int temp = 0;
     while (std::getline(conf_file, line))
     {
+        // get command if existing
         if (line.at(0) == '>')
         {
             command = line;
             command.erase(0, 1);
         }
+        // get path
         else if (line.at(0) == '#')
         {
+            // every time a new path is found, 
+            // push the old vector "links" in to the "_data" vector
             if (temp != 0)
             {
                 _data.push_back(links);
@@ -35,16 +43,16 @@ void config::read(std::string _file)
             links.push_back(line);
         }
     }
+    // push the last vector "links" in to "_data"
     _data.push_back(links);
     
+    // close file
     conf_file.close();
 
 }
 
 void config::replace(std::string *_orig_string, std::string to_replace, std::string replacement)
 {
-    
-
     for (size_t index = 0; (index = _orig_string->find(to_replace)) != std::string::npos;)
         _orig_string->replace(index, to_replace.size(), replacement);
 }
--- src/config.hpp
+++ src/config.hpp
@@ -11,12 +11,16 @@ protected:
 public:
     config(){};
     ~config(){};
+    // reads data from specified file in to the vector "_data"
     void read(std::string _file);
+
+    // in "_orig_string" replaces "to_replace" with "replacement"
     void replace(std::string *_orig_string, std::string to_replace, std::string replacement);
 
+    // command used for executing youtube-dl if specified in "_file"
     std::string command;
-    std::string save_path;
-    std::vector<std::string> links;
+
+    // paths and corresponding links
     std::vector<std::vector<std::string>> _data;
 };
 
