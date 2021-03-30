097ff7472c08347a623712264880ebefabf93dbf
souji <souji@souji-thenria.net>
2021-03-30 20:49:59 +0200

    Ignoring empty lines and lines that are not beginning with

--- src/config.cpp
+++ src/config.cpp
@@ -16,33 +16,37 @@ void config::read(std::string _file)
     int temp = 0;
     while (std::getline(conf_file, line))
     {
-        // get command if existing
-        if (line.at(0) == '>')
+        if (line != "")
         {
-            command = line;
-            command.erase(0, 1);
-        }
-        // get path
-        else if (line.at(0) == '#')
-        {
-            // every time a new path is found, 
-            // push the old vector "links" in to the "_data" vector
-            if (temp != 0)
+            // get command if existing
+            if (line.at(0) == '>')
             {
-                _data.push_back(links);
-                links.clear();
+                command = line;
+                command.erase(0, 1);
             }
+            // get path
+            else if (line.at(0) == '#')
+            {
+                // every time a new path is found, 
+                // push the old vector "links" in to the "_data" vector
+                if (temp != 0)
+                {
+                    _data.push_back(links);
+                    links.clear();
+                }
 
-            save_path = line;
-            save_path.erase(0, 1);
-            links.push_back(save_path);
-            ++temp;
-        }
-        else
-        {
-            links.push_back(line);
+                save_path = line;
+                save_path.erase(0, 1);
+                links.push_back(save_path);
+                ++temp;
+            }
+            else if (line.at(0) == 'h')
+            {
+                links.push_back(line);
+            }
         }
     }
+    
     // push the last vector "links" in to "_data"
     _data.push_back(links);
     
